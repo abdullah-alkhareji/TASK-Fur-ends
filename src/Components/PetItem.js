a7da10d3ea7d7dd4e62e540040887783fd42a626
abdullah-alkhareji <outsiderandco@gmail.com>
2022-01-17 19:33:28 +0300

    Edited changePetImage function name and PetItem props

--- src/Components/PetItem.js
+++ src/Components/PetItem.js
@@ -1,9 +1,8 @@
 import { useState } from "react";
 
-export default function PetItem(props) {
-	const pet = props.pet;
+export default function PetItem({ pet }) {
 	const [image, setImage] = useState(pet.image);
-	const petImage = () => setImage(pet.image2);
+	const changePetImage = () => setImage(pet.image2);
 
 	return (
 		<div className='col-lg-4 col-md-8 col-sm-10'>
@@ -11,12 +10,16 @@ export default function PetItem(props) {
 				<img className='image' alt={pet.name} src={image} />
 				<div className='content'>
 					<h3>{pet.name}</h3>
-					<button onClick={petImage} type='button' className='btn btn-info'>
+					<button
+						onClick={changePetImage}
+						type='button'
+						className='btn btn-info mb-2'>
 						Pet
 					</button>
-					<button onClick={petImage} type='button' className='btn btn-info'>
-						Pet
-					</button>
+					{/* <br />
+					<button onClick={changePetImage} type='button' className='btn btn-info'>
+						Delete
+					</button> */}
 				</div>
 			</div>
 		</div>
